608560c12ba70e13b49a798860da58635db6ad27
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2023-04-17 11:47:42 -0300

    Criando Meu Primeiro Programa em Java

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,15 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um Metodo auto executavel*/
+	public static void main(String[] args) {
+		
+		/*Escreve Texto no Console*/
+		System.out.println("Meu Primeiro Programa em Java!");
+		System.out.println("Java é orientado a objetos!");
+		
+	}
+	
+
+}
