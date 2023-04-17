e036cb5c92ce452c32c185d99be40c28245a0d54
Pedro Lucas <pedrooluucas2010@gmail.com>
2023-04-17 12:07:26 -0300

    Colocando mais um print

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -8,6 +8,7 @@ public class PrimeiraClasseJava {
 		/*Escreve Texto no Console*/
 		System.out.println("Meu Primeiro Programa em Java!");
 		System.out.println("Java é orientado a objetos!");
+		System.out.println("Java é uma das linguagens de programação mais usadas no mundo!");
 		
 	}
 	
